eab36e3b008cd24c6a48a81706bfb44f86fcaf5c
Chokor1 <abbasschokor225@gmail.com>
2025-06-26 16:49:42 +0100

    NCR  report

---
+++ itec_integrations/itec_integrations/report/ncr_price_comparison/ncr_price_comparison.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2025, Abbass Chokor and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["NCR Price Comparison"] = {
+	"filters": [
+
+	]
+};
---
+++ itec_integrations/itec_integrations/report/ncr_price_comparison/ncr_price_comparison.py
@@ -0,0 +1,96 @@
+# Copyright (c) 2025, Abbass Chokor and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+import frappe
+import json
+
+def execute(filters=None):
+    columns = [
+        {"label": "Product Reference", "fieldname": "product_reference", "fieldtype": "Link", "options": "Item", "width": 140},
+        {"label": "NCR Product Name", "fieldname": "ncr_name", "fieldtype": "Data", "width": 300},
+        {"label": "NCR Price", "fieldname": "ncr_price", "fieldtype": "Currency", "width": 120},
+        {"label": "Item Name", "fieldname": "item_name", "fieldtype": "Data", "width": 200},
+        {"label": "Internal Price", "fieldname": "internal_price", "fieldtype": "Currency", "width": 120},
+        {"label": "Difference", "fieldname": "difference", "fieldtype": "Currency", "width": 100},
+        {"label": "Difference (%)", "fieldname": "percent_diff", "fieldtype": "Percent", "width": 120}
+    ]
+
+    data = []
+
+    # Get latest NCR Products doc
+    ncr_doc = frappe.get_all("NCR Products", fields=["name"], order_by="creation desc", limit=1)
+    if not ncr_doc:
+        return columns, data
+
+    ncr_data = frappe.get_doc("NCR Products", ncr_doc[0].name)
+
+    try:
+        products = json.loads(ncr_data.data_json)
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), "Invalid JSON in NCR Products")
+        return columns, data
+
+    sync_setting = frappe.get_single("NCR Sync Setting")
+    tax_category = sync_setting.tax_category
+    for p in products:
+        product_ref = p.get("productReference")
+        ncr_name = p.get("productName")
+        ncr_price = p.get("price") or 0
+
+        # Fetch the corresponding Item
+        item = frappe.db.get_value("Item", {"item_code": product_ref}, ["item_name"], as_dict=True)
+        if item:
+            # Now fetch price from Item Price
+            item_price = frappe.db.get_value(
+                "Item Price",
+                {
+                    "item_code": product_ref,
+                    "price_list": "Standard Selling",
+                    "selling": 1
+                },
+                ["price_list_rate"],
+                as_dict=True
+            )
+
+
+            item_tax = frappe.db.get_value(
+                "Item Tax",
+                {
+                    "parent": product_ref,
+                    "tax_category": tax_category,
+                },
+                [ "item_tax_template"],
+                as_dict=True
+            )
+
+            tax = frappe.db.get_value(
+                "Item Tax Template Detail",
+                {
+                    "parent": item_tax.item_tax_template,
+                },
+                [ "tax_rate"],
+                as_dict=True
+            )
+
+            if item_price:
+                tax_rate = tax.tax_rate or 0
+                internal_price = (item_price.price_list_rate + item_price.price_list_rate*tax_rate/100) or 0
+                difference = internal_price - ncr_price
+                percent_diff = (difference / internal_price * 100) if internal_price else 0
+
+                data.append({
+                    "product_reference": product_ref,
+                    "ncr_name": ncr_name,
+                    "ncr_price": ncr_price,
+                    "item_name": item.item_name,
+                    "internal_price": internal_price,
+                    "difference": difference,
+                    "percent_diff": percent_diff
+                })
+
+    return columns, data
+
+
