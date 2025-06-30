1bbc4d0e684b3b43e8a0aeeccb5c6064dbedf0b6
AMSalameh22 <salameh.ali.dev@gmail.com>
2025-06-30 10:59:02 +0000

    modified:   itec_integrations/itec_integrations/report/ncr_price_comparison/ncr_price_comparison.py

--- itec_integrations/itec_integrations/report/ncr_price_comparison/ncr_price_comparison.py
+++ itec_integrations/itec_integrations/report/ncr_price_comparison/ncr_price_comparison.py
@@ -1,9 +1,6 @@
 # Copyright (c) 2025, Abbass Chokor and contributors
 # For license information, please see license.txt
 
-# import frappe
-
-
 import frappe
 import json
 
@@ -15,12 +12,12 @@ def execute(filters=None):
         {"label": "Item Name", "fieldname": "item_name", "fieldtype": "Data", "width": 200},
         {"label": "Internal Price", "fieldname": "internal_price", "fieldtype": "Currency", "width": 120},
         {"label": "Difference", "fieldname": "difference", "fieldtype": "Currency", "width": 100},
-        {"label": "Difference (%)", "fieldname": "percent_diff", "fieldtype": "Percent", "width": 120}
+        {"label": "Difference (%)", "fieldname": "percent_diff", "fieldtype": "Percent", "width": 120},
+        {"label": "Qty", "fieldname": "qty", "fieldtype": "Float", "width": 100}
     ]
 
     data = []
 
-    # Get latest NCR Products doc
     ncr_doc = frappe.get_all("NCR Products", fields=["name"], order_by="creation desc", limit=1)
     if not ncr_doc:
         return columns, data
@@ -35,27 +32,25 @@ def execute(filters=None):
 
     sync_setting = frappe.get_single("NCR Sync Setting")
     tax_category = sync_setting.tax_category
+
     for p in products:
         product_ref = p.get("productReference")
         ncr_name = p.get("productName")
         ncr_price = p.get("price") or 0
 
-        # Fetch the corresponding Item
         item = frappe.db.get_value("Item", {"item_code": product_ref, "disabled": 0}, ["item_name"], as_dict=True)
         if item:
-            # Now fetch price from Item Price
             item_price = frappe.db.get_all(
                 "Item Price",
                 filters={
-                "item_code": product_ref,
-                "price_list": "Standard Selling",
-                "selling": 1
+                    "item_code": product_ref,
+                    "price_list": "Standard Selling",
+                    "selling": 1
                 },
                 fields=["price_list_rate"],
                 order_by="creation desc",
                 limit=1
-                )
-
+            )
 
             item_tax = frappe.db.get_value(
                 "Item Tax",
@@ -63,22 +58,27 @@ def execute(filters=None):
                     "parent": product_ref,
                     "tax_category": tax_category,
                 },
-                [ "item_tax_template"],
+                ["item_tax_template"],
                 as_dict=True
             )
 
-            tax = frappe.db.get_value(
-                "Item Tax Template Detail",
-                {
-                    "parent": item_tax.item_tax_template,
-                },
-                [ "tax_rate"],
-                as_dict=True
-            )
+            tax_rate = 0
+            if item_tax:
+                tax = frappe.db.get_value(
+                    "Item Tax Template Detail",
+                    {
+                        "parent": item_tax.item_tax_template,
+                    },
+                    ["tax_rate"],
+                    as_dict=True
+                )
+                if tax:
+                    tax_rate = tax.tax_rate or 0
+
+            qty = frappe.db.sql("""SELECT SUM(actual_qty) as qty FROM `tabBin` WHERE item_code = %s""", product_ref, as_dict=True)[0].qty or 0
 
             if item_price:
-                tax_rate = tax.tax_rate or 0
-                internal_price = (item_price[0].price_list_rate + item_price[0].price_list_rate*tax_rate/100) or 0
+                internal_price = item_price[0].price_list_rate + (item_price[0].price_list_rate * tax_rate / 100)
                 difference = internal_price - ncr_price
                 percent_diff = (difference / internal_price * 100) if internal_price else 0
 
@@ -89,9 +89,8 @@ def execute(filters=None):
                     "item_name": item.item_name,
                     "internal_price": internal_price,
                     "difference": difference,
-                    "percent_diff": percent_diff
+                    "percent_diff": percent_diff,
+                    "qty": qty
                 })
 
     return columns, data
-
-
